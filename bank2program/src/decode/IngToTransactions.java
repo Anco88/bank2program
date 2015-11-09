7c6bfbe72f3c3cc7d39685f352ad9192887c9be8
Anco88 <ancogietema@gmx.com>
2015-11-09 16:36:47 +0100

    Deleted extra code

--- bank2program/src/decode/IngToTransactions.java
+++ bank2program/src/decode/IngToTransactions.java
@@ -57,48 +57,4 @@ public class IngToTransactions extends CSVToTransactions {
 				category, ownAccount, account, code);
 	}
 
-
-
-	/*@Override
-	public Transaction readFromLine(String str, int n, Transaction t) {
-		
-		/* 
-		 *1 "Datum",
-		2"Naam / Omschrijving",
-		3"Rekening",
-		4"Tegenrekening",
-		5"Code",
-		6"Af Bij",
-		7"Bedrag (EUR)",
-		8"MutatieSoort",
-		9"Mededelingen"
-		*/
-		// skip first line
-	/*
-		if(n==1){
-			return null;
-		}
-
-		str = str.substring(1, str.length() - 1);
-		String[] strings = str.split("(\",\")");
-	
-		t.setDate(strings[0]);
-		t.setName (strings[1]);
-		t.setOwnAccount(strings[2]);
-		t.setAccount(strings[3]);
-		t.setCode(strings[4]);
-		
-		if (strings[5].contains("A")) {
-			t.setDC("-");
-		}
-
-		t.setAmount(strings[6]);
-		t.setCurrency("euro");		
-		t.setDescription(strings[8]);
-		
-		t.setComment("");
-		t.setCategory(rules.findCategory(t));
-		return t;
-	}*/
-
 }
--- bank2program/src/decode/RaboToTransactions.java
+++ bank2program/src/decode/RaboToTransactions.java
@@ -20,41 +20,6 @@ public class RaboToTransactions extends CSVToTransactions{
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	public Transaction readFromLine(String str, int n, Transaction t) {
-	//	Transaction t = new Transaction();
-		String description = "";
-		String comment = "";
-		str = str.substring(1, str.length() - 1);
-		String[] strings = str.split("(\",\")");
-		t.setOwnAccount(strings[0]);
-		t.setCurrency(strings[1]);
-
-		if (strings[3].contains("D")) {
-			t.setDC("-");
-		}
-
-		t.setAmount(strings[4]);
-
-		t.setAccount(strings[5]);
-	//	t.setName (strings[6]);
-		t.setDate(strings[7]);
-		t.setCode(strings[8]);
-
-		if (strings.length > 10) {
-			description = strings[10];
-		}
-		
-	//	t.setDescription(description);
-		
-		for (int i = 11; i < strings.length; i++) {
-			comment += " " + strings[i];
-		}
-		comment = comment.trim();
-		t.setComment(comment);
-	//	t.setCategory(rules.findCategory(t));
-		return t;
-	}
 
 	@Override
 	Transaction readFromLine(String str, int number) {
